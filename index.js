6d946edc456f4d33e18a8d4eb937a4d1da163b08
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-08-17 14:10:36 -0400

    Completed index.js as indicated

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Speedy Gonzalez",
+    streetaddress : "1008 Bayfield Street",
+    };
+function updateEmployeeWithKeyAndValue(employee,name, Juan){
+    const newEmployee = { ...employee };
+    newEmployee[name] = Juan;
+    return newEmployee;
+  }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name,Juan){
+    employee[name] = Juan
+    return employee
+}
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name 
+    return employee;
+}
+function deleteFromEmployeeByKey(){
+    const newEmployee = { ...employee };
+    delete newEmployee.name 
+    return newEmployee;
+
+}
